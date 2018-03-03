02535c05e3aefe22615dfea0c79839c941896938
honmaple <xiyang0807@gmail.com>
2018-03-03 22:01:33 +0800

    avatar fixed with flask_avatar 0.1.3

--- forums/extension/__init__.py
+++ forums/extension/__init__.py
@@ -6,11 +6,11 @@
 # Author: jianglin
 # Email: xiyang0807@gmail.com
 # Created: 2018-02-11 14:52:12 (CST)
-# Last Update: 星期日 2018-02-11 15:31:19 (CST)
+# Last Update: Saturday 2018-03-03 21:53:59 (CST)
 #          By:
 # Description:
 # ********************************************************************************
-from flask import request, current_app
+from flask import request
 from flask_wtf.csrf import CSRFProtect
 from flask_avatar import Avatar
 from flask_maple.models import db
@@ -28,31 +28,14 @@ cache = Cache()
 mail = Mail()
 principal = Principal()
 search = Search(db=db)
-
-
-class AvatarCache(Avatar):
-    @cache.cached(
-        timeout=180, key_prefix=lambda: "avatar:{}".format(request.url))
-    def avatar(self, text, width=128):
-        from flask import abort, make_response
-        from flask_avatar.avatar import GenAvatar
-        width_range = current_app.config.get('AVATAR_RANGE', [0, 512])
-        if width < width_range[0] or width > width_range[1]:
-            abort(404)
-        stream = GenAvatar.generate(width, text)
-        buf_value = stream.getvalue()
-        response = make_response(buf_value)
-        response.headers['Content-Type'] = 'image/jpeg'
-        return response
-
-
-avatar = AvatarCache()
+avatar = Avatar(cache=cache.cached(
+    timeout=259200, key_prefix=lambda: "avatar:{}".format(request.url)))
 
 
 def init_app(app):
     db.init_app(app)
-    avatar.init_app(app)
     cache.init_app(app)
+    avatar.init_app(app)
     csrf.init_app(app)
     principal.init_app(app)
     redis_data.init_app(app)
